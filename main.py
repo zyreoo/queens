0a1e879755984fd94fb24f16fe3889be58e7cccf
zyreoo <simone.marton89@gmail.com>
2025-05-03 10:26:12 +0300

    to see why it s not working ndasda

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+
+
+# The beggining. In the first place i need to give to people 4 cards, that have been shuffled.
+
+# 1. i need to see how many players i will have n my game. 
+# 2. i need to give each player 4 cards i think i will have an 12 max capacity, or maybe 18 i will think about it, but for now i will only be a 4 game
+
+
